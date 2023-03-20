97c8a6ddc077af8c3c68ceb15666eb6ddc336f4d
Parth Parikh <parthparikh1999p@gmail.com>
2023-03-20 11:42:19 -0400

    Large text is now broken down into smaller chunks Each input request has a max of around 1900 tokens

--- grammatical.py
+++ grammatical.py
@@ -19,16 +19,30 @@ def verify_api_key(api_key):
 
 def verify_text_length(text):
     """
-    Verify that the text is less than 2000 tokens,
-    as the output will also require around 2000 tokens.
+    Verify that the text is less than 1900 tokens,
+    as the output will also require around 1900 tokens.
     From the OpenAI documentation:
     https://help.openai.com/en/articles/4936856-what-are-tokens-and-how-to-count-them
     1 token ~= 4 chars in English
     """
-    if len(text) / 4 > 2000:
-        raise Exception(
-            "Text is too long. Please keep it under 2000 tokens or 8000 characters."
-        )
+    if len(text) / 4 > 1900:
+        paragraphs = text.split("\n\n")
+        chunks = list()
+        chunks.append(str())
+        # Squeeze in as many paragraphs as possible in a single chunk
+        for paragraph in paragraphs:
+            if len(paragraph) / 4 > 1900:
+                raise Exception(
+                    "Text is too long. Please keep each paragraph under 1900 tokens or 7600 characters."
+                    "If you have a lot of text, please split it into multiple paragraphs."
+                    "Each paragraph should be separated by a newline."
+                )
+            if len(chunks[-1]) / 4 <= 1900:
+                chunks[-1] += " \n\n " + paragraph
+            else:
+                chunks.append(paragraph)
+        return chunks
+    return [text]
 
 
 @click.command()
@@ -60,28 +74,31 @@ def grammatical(text, api_key, tone, simplify):
     Corrects the spelling and grammar of the text.
     """
     openai.api_key = verify_api_key(api_key)
-    verify_text_length(text)
-    simplify_text = (
-        "Can you also simplify the complex sentences "
-        "for better clarity? Keep the meaning intact."
-        if simplify
-        else ""
-    )
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "user",
-                "content": f"Can you improve the grammar of the following text?"
-                f"And correct the spelling of the words, if necessary?"
-                f"{simplify_text}"
-                f"I need it for {tone}, so keep the tone specific to that."
-                f"\n\n Text is: {text}",
-            },
-        ],
-    )
-    modified_text = response.choices[0]["message"]["content"]
-    rich_print(modified_text)
+    chunks = verify_text_length(text)
+    modified_texts = list()
+    for chunk in chunks:
+        simplify_text = (
+            "Can you also simplify the complex sentences "
+            "for better clarity? Keep the meaning intact."
+            if simplify
+            else ""
+        )
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "user",
+                    "content": f"Can you improve the grammar of the following text?"
+                    f"And correct the spelling of the words, if necessary?"
+                    f"{simplify_text}"
+                    f"I need it for {tone}, so keep the tone specific to that."
+                    f"\n\n Text is: {chunk}",
+                },
+            ],
+        )
+        resp = response.choices[0]["message"]["content"]
+        modified_texts.append(resp)
+    rich_print("\n\n".join(modified_texts))
 
 
 def rich_print(text):
